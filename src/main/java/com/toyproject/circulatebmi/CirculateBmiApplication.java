17de86f2dbcf981d1821d5aa659b7fc91dd7dd55
wonjuYang <his1monologue@gmail.com>
2022-05-08 16:22:47 +0900

    기초 세팅

---
+++ src/main/java/com/toyproject/circulatebmi/CirculateBmiApplication.java
@@ -0,0 +1,15 @@
+package com.toyproject.circulatebmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirculateBmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CirculateBmiApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/com/toyproject/circulatebmi/CirculateBmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.circulatebmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CirculateBmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
